32e5dfbacb49358e9fcd99ea68dbe213437c3935
villegasleon <109755324+villegasleon@users.noreply.github.com>
2023-09-15 12:28:42 -0600

    Card aplicando useEffect useState
    Aqui preparamos nuestro index.jsx donde importamos useState y useEffect . Trabajamos con promises (fetch y .then) todo esto en Home, pintando la card que trae la informacion de los items

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,12 +1,26 @@
+import { useEffect, useState } from 'react'
 import Layout from '../../Components/Layout'
 import Card from '../../Components/Card'
 
 function Home() {
+  const [items, setItems] = useState(null)
+
+  useEffect(()=>{
+    fetch('https://api.escuelajs.co/api/v1/products')
+    .then(response=>response.json())
+    .then(data=>setItems(data))
+
+  }, [])
+
   return (
 
       <Layout>
         Home
-        <Card />
+        {
+          items?.map(()=>{
+            return <Card/>
+          })
+        }
       </Layout>
   )
 }
