dd735ddbb310c0b1a7e2d0bf866d90cc04756378
villegasleon <109755324+villegasleon@users.noreply.github.com>
2023-09-25 13:35:59 -0600

    close vista detallada
    Ya podemos ver imagen, tittulo descripcion y tambien podemos cerrar la vista detallada con el icono "x"

--- src/Components/ProductDetail/index.jsx
+++ src/Components/ProductDetail/index.jsx
@@ -1,21 +1,37 @@
 import { useContext } from 'react';
-import { IconBrandX } from '@tabler/icons-react';
+import { IconX } from '@tabler/icons-react';
 import './styles.css'
 import { ShoppingCartContext } from '../../Context';
 
 const ProductDetail = ()=>{
     const context= useContext(ShoppingCartContext)
-    console.log('PRODUCT TO SHOW :', context.productToShow)
+    // console.log('PRODUCT TO SHOW :', context.productToShow)
 
     return(
         <aside 
-        className={`${context.isProductDetailOpen ? 'flex' : 'hidden'} product-detail flex flex-col fixed right-0 border border-black rounded-lg bg-white`}>
+        className={`${context.isProductDetailOpen ? 'flex' : 'hidden'} product-detail flex-col fixed right-0 border border-black rounded-lg bg-white`}>
             <div className='flex justify-between items-center p-6'>
                 <h2 className='font-medium text-xl'>Detail</h2>
+                {/* En el icono de la "x" para cerrar la vista detallada, agregamos un onClick que trae del contexto
+                la logica para cerrar la ventana  */}
                 <div>
-                    <IconBrandX color="black" size={15}></IconBrandX>
+                    <IconX color="black" size={10} className='h-6 w-6 text-black'
+                    onClick={()=> context.closeProductDetail()}></IconX>
                 </div>
             </div>
+                {/*Aqui creamos nuestra figure, aqui va a pintar el titulo y la imagen  */}
+                <figure className='px-6'>
+                    <img className='w-full h-full rounded-lg' 
+                    src= {context.productToShow.images} 
+                    alt={context.productToShow.title}/> 
+                </figure>
+                {/*Aqui creamos el texto q llevara la card como precio, titulo y descripcion  */}
+                <p className='flex flex-col p-6'>
+                    <span className='font-medium text-2xl'>${context.productToShow.price}</span>
+                    <span className='font-medium text-md'>${context.productToShow.title}</span>
+                    <span className='font-light text-sm'>${context.productToShow.description}</span>
+                </p>
+
 
         </aside>
 
